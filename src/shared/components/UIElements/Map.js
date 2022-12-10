e552e01a87c00728c3432807da2d0884e65a41a8
Anubhav Jain <anubhav823@gmail.com>
2022-12-10 23:44:47 +0530

    Update Map.js

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -8,18 +8,6 @@ const Map = props => {
     const { center, zoom } = props;
 
     useEffect(() => {
-        new window.ol.Map({
-            target: mapRef.current.id,
-            layers: [
-                new window.ol.layer.Tile({
-                    source: new window.ol.source.OSM()
-                })
-            ],
-            view: new window.ol.View({
-                center: window.ol.proj.fromLonLat([center.lng, center.lat]),
-                zoom: zoom
-            })
-        });
     }, [center, zoom]);
 
     return (
